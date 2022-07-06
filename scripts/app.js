6d459a80fee8cffabfe3123a9be986117552aa4d
SmartphonE <pratyakshsoni2004@gmail.com>
2022-07-06 12:46:57 +0530

    Update About Me page

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,26 @@
-window.onload = function(){
-    var x = document.querySelectorAll('#LogsSection div')
-    x[0].textContent = "Screen Height: " + screen.availHeight
-    x[1].textContent = "Screen Width: " + screen.availWidth
 
-}
+// window.onload = function(){
+
+//     var x = document.querySelector('body')
+//     x.innerHTML += categorySection(["tag1","tag2","tag3","tag4","tag5"])
+
+// }
+
+// function categorySection( tagsName ){
+
+//     var newSectionHtml= "" +
+//                    ` <section class="myPassionSection">
+//                     <div class="sectionHeadingLabels">My JS Generates</div>
+
+                    
+//                 </section>`
+
+//     tagsName.forEach(element => {
+
+//         newSectionHtml+= ` <div id="languagesDivLabel" style=" background-image: linear-gradient(to right,rgb(51, 255, 0), rgb(0, 255, 115));"> ${element} </div>`
+
+//     });
+
+//     return newSectionHtml
+
+// }
